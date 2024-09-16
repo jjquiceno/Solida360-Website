ba168268fcefd7e2489d07464d31c649dffc6b2b
juan José Quiceno Londoño <quicenolondonoj@gmail.com>
2024-09-16 12:02:08 -0500

    slider de productos, y estilos adicionales

--- front-end/solidacomercial/productos.php
+++ front-end/solidacomercial/productos.php
@@ -109,6 +109,13 @@
 
     </div>
     <section class="productDetail">
+        <div class="volver">
+            <a href="homecomer.html">
+                <div>
+                    <i class="fa-solid fa-arrow-left fa-2x" style="color: #b8c66c;"></i>
+                </div>
+            </a>
+        </div>
         <div class="productImg">
             <img src="../img/gf-imp3.jpg" alt="">
         </div>
@@ -240,7 +247,201 @@
             </script>
         </div>
     </section>
-    
+    <section class="atuMedida">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#b8c66c" fill-opacity="1" d="M0,64L48,96C96,128,192,192,288,197.3C384,203,480,149,576,106.7C672,64,768,32,864,53.3C960,75,1056,149,1152,181.3C1248,213,1344,203,1392,197.3L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+    </section>
+    <div class="text-recomendaciones">
+        <div class="int-text-recom">
+            <h2 class="tittles"><span class="light">¡Recomendaciones</span>Para Ti!</h2>
+        </div>
+    </div>
+    <br>
+    <div class="separador-hor"></div>
+    <section class="recomendaciones">
+        <div class="slider">
+            <div class="slides">
+                <div class="slide"><img src="image1.jpg" alt="Imagen 1"></div>
+                <div class="slide"><img src="image2.jpg" alt="Imagen 2"></div>
+                <div class="slide"><img src="image3.jpg" alt="Imagen 3"></div>
+                <div class="slide"><img src="image4.jpg" alt="Imagen 4"></div>
+                <div class="slide"><img src="image5.jpg" alt="Imagen 5"></div>
+                <div class="slide"><img src="image6.jpg" alt="Imagen 6"></div>
+                <div class="slide"></div>
+                <div class="slide"></div>
+                <div class="slide"></div>
+                <div class="slide"></div>
+            </div>
+            <div class="controls">
+                <button class="control prev"><i class="fa-solid fa-arrow-right fa-flip-both fa-2x" style="color: black;"></i></button>
+                <button class="control next"><i class="fa-solid fa-arrow-right fa-2x" style="color: black;"></i></button>
+            </div>
+            <div class="indicators">
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+                <div class="indicator"></div>
+            </div>
+        </div>
+        <script>
+            let index = 0;
+            const slides = document.querySelector('.slides');
+            const totalSlides = document.querySelectorAll('.slide').length;
+            const prevButton = document.querySelector('.prev');
+            const nextButton = document.querySelector('.next');
+            const indicators = document.querySelectorAll('.indicator');
+
+            function updateSlider() {
+                slides.style.transform = `translateX(${-index * 30}%)`;
+                indicators.forEach((indicator, i) => {
+                    indicator.classList.toggle('active', i === index);
+                });
+            }
+
+            prevButton.addEventListener('click', () => {
+                index = (index - 1 + totalSlides) % totalSlides;
+                updateSlider();
+            });
+
+            nextButton.addEventListener('click', () => {
+                index = (index + 1) % totalSlides;
+                updateSlider();
+            });
+
+            indicators.forEach((indicator, i) => {
+                indicator.addEventListener('click', () => {
+                    index = i;
+                    updateSlider();
+                });
+            });
+
+            let autoSlide = setInterval(() => {
+                index = (index + 1) % totalSlides;
+                updateSlider();
+            }, 3000);
+
+            document.querySelector('.slider').addEventListener('mouseover', () => {
+                clearInterval(autoSlide);
+            });
+
+            document.querySelector('.slider').addEventListener('mouseout', () => {
+                autoSlide = setInterval(() => {
+                    index = (index + 1) % totalSlides;
+                    updateSlider();
+                }, 3000);
+            });
+
+            updateSlider();
+        </script>
+    </section>
+    <section class="footter">
+        <div class="foot-interior">
+            <div class="fot-info" id="info1">
+                <div>
+                    <img src="../img/iconos/ipbe.png" alt="" class="logo-solida-fot">
+                </div>
+                <div>
+                    <p class="">Somos Sólida, una empresa apasionada por la creatividad y la innovación en el mundo de las comunicaciones, el mercadeo y la producción de impresos.</p>
+                </div>
+            </div>
+            <div class="fot-info" id="info2">
+                <div class="fot-titulos-internos">
+                    <h3>MARCAS <span class="light">SÓLIDAS</span></h3>
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="#posicion">MONTECASINO</a></li>
+                        <li><a href="#posicion">CORPOASES</a></li>
+                        <li><a href="#posicion">FUNDACIÓN HUELLAS DEL AYER</a></li>
+                        <li><a href="#posicion">ARRULLA</a></li>
+                        <li><a href="#posicion">ONDAS</a></li>
+                        <li><a href="#posicion">AURUM</a></li>                            <li><a href="#posicion">SPOT SERVICIOS</a></li>
+                        <li><a href="#posicion">CENTRO EDUCATIVO MELODÍAS</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="fot-info" id="info3">
+                <div class="fot-titulos-internos">
+                    <h3>CONTACTO</h3>
+                </div>
+                <div>
+                    <ul>
+                        <div class="fot-flex">
+                            <div>
+                                <img src="front-end/img/iconos/ubi.png" alt="" class="footer-iconos">
+                            </div>
+                            <div>
+                                <a href="https://maps.app.goo.gl/NkUMMBjKmGbwSTrk6" target="_blank"><p>Cra. 41A #27 A sur 86 - Centro Ejecutivo La Casona Envigado - Oficina 102</p></a>
+                            </div>
+                        </div>
+                        <div class="fot-flex">
+                            <div>
+                                <img src="front-end/img/iconos/mail.png" alt="" class="footer-iconos">
+                            </div>
+                            <div>
+                                <p>gestion@solidasas.com</p>
+                            </div>
+                        </div>
+                        <div class="fot-flex">
+                            <div>
+                                <img src="front-end/img/iconos/wats.png" alt="" class="footer-iconos">
+                            </div>
+                            <div>
+                                <!--<a href=""><p>+57 324 569 36 94</p></a>-->
+                                <p>+57 324 569 36 94</p>
+                            </div>
+                        </div>
+                        <div class="fot-flex">
+                            <div>
+                                <img src="front-end/img/iconos/time.png" alt="" class="footer-iconos">
+                            </div>
+                            <div>
+                                <p>Lunes a viernes de 8:00 a.m. a 5:00 p.m.</p>
+                            </div>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+            <div class="fot-info" id="info4">
+                <div class="fot-titulos-internos">
+                    <h3>SÍGUENOS</h3>
+                </div>
+                <div>
+                    <div class="fot-flex">
+                        <div>
+                            <img src="front-end/img/iconos/in.png" alt="" class="footer-iconos">
+                        </div>
+                        <div>
+                            <a href="https://co.linkedin.com/company/s%C3%B3lida-sas" target="_blank"><p>Linkedin</p></a><br>
+                            <!--<a href="https://www.instagram.com/solida360_?igsh=MWFlZW9iaTJ0N3NuMQ%3D%3D"><p>Instagram</p></a>-->
+                            <!--<a href="https://www.tiktok.com/@solida.360?_t=8mP23a76KLV&_r=1"><p>Tik Tok</p></a>-->
+                        </div>
+                    </div>
+                    <div class="fot-flex">
+                        <div>
+                            <img src="front-end/img/iconos/igblanco.png" alt="" class="footer-iconos">
+                        </div>
+                        <div>
+                            <a href="https://www.instagram.com/solida360_?igsh=MWFlZW9iaTJ0N3NuMQ%3D%3D" target="_blank"><p>Instagram</p></a>
+                        </div>
+                    </div>
+                    <div class="fot-flex">
+                        <div>
+                            <img src="front-end/img/iconos/ttblanco.png" alt="" class="footer-iconos">
+                        </div>
+                        <div>
+                            <a href="https://www.tiktok.com/@solida.360?_t=8mP23a76KLV&_r=1" target="_blank"><p>Tik Tok</p></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
     	<script>
         	AOS.init()
